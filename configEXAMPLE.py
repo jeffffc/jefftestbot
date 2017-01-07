25fa7ef7396e2fd0592cab92f618579b2f6c2089
Jeff Choy <jeffchoy94610@gmail.com>
2017-01-07 07:35:58 +0000

    some more..

--- configEXAMPLE.py
+++ configEXAMPLE.py
@@ -38,9 +38,14 @@ GOOGLE_API = ""
 # OPTAINED AT https://cse.google.com/
 CSE_ID = ""
 
+###YANDEX API
+YANDEX_API = ""
+
 ### CREATE TABLE SQL, DO NOT AMEND###
 SQL_CREATE_TABLE_1 = "CREATE TABLE IF NOT EXISTS `feedback` (`id` int(11) NOT NULL AUTO_INCREMENT, `message` mediumtext NOT NULL, `name` varchar(200) NOT NULL, `username` varchar(100) NOT NULL, `telegramid` int(20) NOT NULL, PRIMARY KEY (`id`), UNIQUE KEY `id` (`id`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4"
 SQL_CREATE_TABLE_2 = "CREATE TABLE IF NOT EXISTS `group` (`id` int(11) NOT NULL AUTO_INCREMENT, `name` varchar(150) NOT NULL, `groupid` bigint(30) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4"
 SQL_CREATE_TABLE_3 = "CREATE TABLE IF NOT EXISTS `patdb` (`patid` int(11) NOT NULL AUTO_INCREMENT, `patdesc` text NOT NULL, PRIMARY KEY (`patid`), UNIQUE (`patid`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4"
-SQL_CREATE_TABLE_4 = "CREATE TABLE IF NOT EXISTS `user` (`userid` int(11) NOT NULL AUTO_INCREMENT, `name` varchar(100) NOT NULL, `username` varchar(100) NOT NULL, `telegramid` int(20) NOT NULL, `patted` int(11) NOT NULL, `pattedby` int(11) NOT NULL, `loc` varchar(50) DEFAULT NULL, PRIMARY KEY (`userid`), UNIQUE KEY `userid` (`userid`, `telegramid`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4"
+SQL_CREATE_TABLE_4 = "CREATE TABLE IF NOT EXISTS `user` (`userid` int(11) NOT NULL AUTO_INCREMENT, `name` varchar(100) NOT NULL, `username` varchar(100) DEFAULT NULL, `telegramid` int(20) NOT NULL, `patted` int(11) NOT NULL DEFAULT '0', `pattedby` int(11) NOT NULL DEFAULT '0', `loc` varchar(50) DEFAULT NULL, `banned` tinyint(1) NOT NULL DEFAULT '0', PRIMARY KEY (`userid`), UNIQUE KEY `userid` (`userid`), `telegramid` (`telegramid`)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4"
 SQL_DEFAULT_PAT = "INSERT INTO `patdb` (`patid`, `patdesc`) VALUES (1, 'is patted by'), (2, 'is again patted by')"
+
+
--- trans.py
+++ trans.py
@@ -1,26 +1,11 @@
-#!/bin/bash
-#import sys
-#import os
-#import subprocess
-#import goslate
-#from translate import translator
-#from googletrans import translator
 from yandex_translate import YandexTranslate
+from config import *
 
-translate = YandexTranslate('trnsl.1.1.20170105T150217Z.672e65c941ac42b8.0ddaa2677d1d56a71c4f9833191efe39e4b02f63')
-#gs = goslate.Goslate()
+translate = YandexTranslate(YANDEX_API)
 
 def trans(text):
-#    return translator('en', text)
-#    cmd = "trans :en %s" % text
-#    cmd = subprocess.call(["translate", ":en", text])
-#    test = cmd
-#    print(test)
-#    output = translator.translate(text)
-#    return output
     inputlang = translate.detect(text)
     output = translate.translate(text, 'en')
     outputtext = output['text'][0]
     return inputlang, outputtext
 
-#trans("你好, 做緊咩呀")
